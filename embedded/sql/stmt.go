10d0f12101057726110df9d43ea4c1d34f48d260
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2022-01-17 20:27:44 -0300

    chore(embedded/sql): functional catalog api
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

--- embedded/sql/stmt.go
+++ embedded/sql/stmt.go
@@ -3501,6 +3501,7 @@ func (stmt *ListDatabasesStmt) Resolve(tx *SQLTx, params map[string]interface{},
 }
 
 type ListTablesStmt struct {
+	db string
 	as string
 }
 
@@ -3519,14 +3520,25 @@ func (stmt *ListTablesStmt) Alias() string {
 	return stmt.as
 }
 
-func (stmt *ListTablesStmt) Resolve(tx *SQLTx, params map[string]interface{}, ScanSpecs *ScanSpecs) (RowReader, error) {
+func (stmt *ListTablesStmt) Resolve(tx *SQLTx, params map[string]interface{}, ScanSpecs *ScanSpecs) (rowReader RowReader, err error) {
 	cols := make([]ColDescriptor, 1)
 	cols[0] = ColDescriptor{
 		Column: "name",
 		Type:   VarcharType,
 	}
 
-	tables := tx.Database().GetTables()
+	var db *Database
+
+	if stmt.db != "" {
+		db, err = tx.catalog.GetDatabaseByName(stmt.db)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		db = tx.Database()
+	}
+
+	tables := db.GetTables()
 
 	values := make([][]ValueExp, len(tables))
 
@@ -3538,8 +3550,8 @@ func (stmt *ListTablesStmt) Resolve(tx *SQLTx, params map[string]interface{}, Sc
 }
 
 type ListColumnsStmt struct {
-	table string
-	as    string
+	tableRef *tableRef
+	as       string
 }
 
 func (stmt *ListColumnsStmt) execAt(tx *SQLTx, params map[string]interface{}) (*SQLTx, error) {
@@ -3589,7 +3601,7 @@ func (stmt *ListColumnsStmt) Resolve(tx *SQLTx, params map[string]interface{}, S
 		},
 	}
 
-	table, err := tx.Database().GetTableByName(stmt.table)
+	table, err := stmt.tableRef.referencedTable(tx)
 	if err != nil {
 		return nil, err
 	}
@@ -3631,8 +3643,8 @@ func (stmt *ListColumnsStmt) Resolve(tx *SQLTx, params map[string]interface{}, S
 }
 
 type ListIndexesStmt struct {
-	table string
-	as    string
+	tableRef *tableRef
+	as       string
 }
 
 func (stmt *ListIndexesStmt) execAt(tx *SQLTx, params map[string]interface{}) (*SQLTx, error) {
@@ -3665,7 +3677,7 @@ func (stmt *ListIndexesStmt) Resolve(tx *SQLTx, params map[string]interface{}, S
 		Type:   BooleanType,
 	}
 
-	table, err := tx.Database().GetTableByName(stmt.table)
+	table, err := stmt.tableRef.referencedTable(tx)
 	if err != nil {
 		return nil, err
 	}
